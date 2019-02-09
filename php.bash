718090a3acf7d297e47cfa5f3b8e120b469389a7
Duane Whitty <duane@nofroth.com>
2019-02-08 21:45:19 -0400

    Pull files from the default free Docker registry

--- php.bash
+++ php.bash
@@ -1,3 +1,3 @@
-docker run --rm -d -p 8080:80 -v /c/Users/LENOVO/dev/docker/php/docker-php-dev/source/:/var/www/html/ --name php-server php-base:1.3
-docker run --rm -ti -v /c/Users/LENOVO/dev/docker/php/docker-php-dev/:/home/duanewhitty --name php-edit php-vim:1.7 bash
+docker run --rm -d -p 8080:80 -v /c/Users/LENOVO/dev/docker/php/docker-php-dev/source/:/var/www/html/ --name php-server dwhitty/php-base:1.3
+docker run --rm -ti -v /c/Users/LENOVO/dev/docker/php/docker-php-dev/:/home/duanewhitty --name php-edit dwhitty/php-vim:1.7 bash
 docker container stop php-server
